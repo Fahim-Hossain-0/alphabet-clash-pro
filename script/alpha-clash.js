7250f03d662925f0b3fa872df3b9e249eaf3d861
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-18 02:18:36 +0600

    add

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,4 +1,10 @@
 function play(){
     hideElementsById('home-screen')
     showElementById('play-ground')
+    continueGame()
+}
+function continueGame(){
+    const alphabet = getRandomAlphabets()
+    const currentAlphabet = document.getElementById("current-alphabets")
+    currentAlphabet.innerText = alphabet
 }
--- script/utilits.js
+++ script/utilits.js
@@ -1,9 +1,18 @@
-function hideElementsById(element){
+function hideElementsById(element) {
     const hideElement = document.getElementById(element)
     hideElement.classList.add('hidden')
 }
 
-function showElementById(element){
+function showElementById(element) {
     const showElement = document.getElementById(element)
     showElement.classList.remove('hidden')
+}
+
+function getRandomAlphabets() {
+    const alphabetString = "ABCDEFGHIRJKLMNOPQSTUVWXYZ"
+    const alphabets = alphabetString.split('')
+    const randomNumber = Math.random() * 25
+    const index = Math.round(randomNumber)
+    const alphabet = alphabets[index]
+    return alphabet
 }
