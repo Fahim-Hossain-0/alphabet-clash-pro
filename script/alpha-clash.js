8ec5c6fb6c4d13de3891a875a0c3b8634b44fa63
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-19 02:09:48 +0600

    color could not set successfully

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,10 +1,13 @@
+
+function continueGame(){
+    const alphabet = getRandomAlphabet()
+    console.log(alphabet )
+    const currentAlphabet = document.getElementById("current-alphabet")
+    currentAlphabet.innerText = alphabet
+    setBackgroundColorById(alphabet) 
+}
 function play(){
     hideElementsById('home-screen')
     showElementById('play-ground')
     continueGame()
-}
-function continueGame(){
-    const alphabet = getRandomAlphabets()
-    const currentAlphabet = document.getElementById("current-alphabets")
-    currentAlphabet.innerText = alphabet
 }
--- script/utilits.js
+++ script/utilits.js
@@ -1,18 +1,25 @@
-function hideElementsById(element) {
-    const hideElement = document.getElementById(element)
-    hideElement.classList.add('hidden')
+function hideElementsById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('hidden')
 }
 
-function showElementById(element) {
-    const showElement = document.getElementById(element)
-    showElement.classList.remove('hidden')
+function showElementById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden')
 }
 
-function getRandomAlphabets() {
+function getRandomAlphabet() {
     const alphabetString = "ABCDEFGHIRJKLMNOPQSTUVWXYZ"
     const alphabets = alphabetString.split('')
     const randomNumber = Math.random() * 25
     const index = Math.round(randomNumber)
-    const alphabet = alphabets[index]
+    const alphabet = alphabets[index]   
+    // console.log(index,alphabet)
     return alphabet
+
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
 }
