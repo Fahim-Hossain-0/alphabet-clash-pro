fd88916c8d947302dc60b903942ce7b297536fa7
Fahim-Hossain-0 <fahimhossain735@gmail.com>
2024-09-21 02:15:02 +0600

    score and life update

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,29 +1,37 @@
-function handleKeyboardButtonPress(event){
+function handleKeyboardButtonPress(event) {
     const playerPressed = event.key
     const currentAlphabetElement = document.getElementById('current-alphabet')
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase()
-    
-    if(playerPressed === expectedAlphabet){
+
+    if (playerPressed === expectedAlphabet) {
+        const currentScoreElement = document.getElementById('current-score')
+        const currentScoreText = currentScoreElement.innerText
+        const currentScore = parseInt(currentScoreText)
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+
         removeBackgroundColorById(expectedAlphabet)
-        continueGame(expectedAlphabet)
+        continueGame()
     }
-    else{
-        console.log("wrrong key press")
+    else {
+        const currentLifeElement = document.getElementById('curren-life')
+        const currentLifeText = currentLifeElement.innerText
+        const currentText = parseInt(currentLifeText)
+        const newLife = currentText - 1
+        currentLifeElement.innerText = newLife
     }
-
-
 }
 
-document.addEventListener('keyup',handleKeyboardButtonPress)
+document.addEventListener('keyup', handleKeyboardButtonPress)
 
-function continueGame(){
+function continueGame() {
     const alphabet = getARandomAlphabet();
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
     setBackgroundColorById(alphabet);
 }
-function play(){
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();
